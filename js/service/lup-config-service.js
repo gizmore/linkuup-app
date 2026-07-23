dc0ae6ab36147e1f9e2d59a88b8e17d2943d4be9
gizmore <gizmore@wechall.net>
2026-07-23 12:49:45 +0200

    Normalize boolean configuration values

--- js/service/lup-config-service.js
+++ js/service/lup-config-service.js
@@ -31,50 +31,54 @@ service('ConfigSrvc', function(RequestSrvc) {
 		return result;
 	};
 
+	ConfigSrvc.bool = function(value) {
+		return value === true || value === 1 || value === '1';
+	};
+
 	ConfigSrvc.signupCaptcha = function() {
 		var result = ConfigSrvc.CACHE.Register.captcha;
 //		console.log('ConfigSrvc.signupCaptcha() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	
 	ConfigSrvc.tosForced = function() {
 		var result = ConfigSrvc.CACHE.Register.force_tos;
 //		console.log('ConfigSrvc.tosForced() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 
 	ConfigSrvc.emailActivation = function() {
 		var result = ConfigSrvc.CACHE.Register.email_activation;
 //		console.log('ConfigSrvc.emailActivation() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	
 	ConfigSrvc.guestAvatars = function() {
 		var result = ConfigSrvc.CACHE.Avatar.avatar_guests;
 //		console.log('ConfigSrvc.guestAvatars() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	
 	ConfigSrvc.passwordRetype = function() {
 		var result = ConfigSrvc.CACHE.Register.signup_password_retype;
 		console.log('ConfigSrvc.passwordRetype() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 
 	ConfigSrvc.recoveryCaptcha = function() {
 		var result = ConfigSrvc.CACHE.Recovery.recovery_captcha;
 		console.log('ConfigSrvc.recoveryCaptcha() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	ConfigSrvc.recoveryEmail = function() {
 		var result = ConfigSrvc.CACHE.Recovery.recovery_email;
 		console.log('ConfigSrvc.recoveryEmail() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	ConfigSrvc.recoveryLogin = function() {
 		var result = ConfigSrvc.CACHE.Recovery.recovery_login;
 		console.log('ConfigSrvc.recoveryLogin() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	
 	ConfigSrvc.ipp = function() {
@@ -86,7 +90,7 @@ service('ConfigSrvc', function(RequestSrvc) {
 	ConfigSrvc.singleACL = function() {
 		var result = ConfigSrvc.CACHE.Profile.profile_single_acl;
 		console.log('ConfigSrvc.singleACL() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	
 	/////////////
@@ -95,45 +99,45 @@ service('ConfigSrvc', function(RequestSrvc) {
 	ConfigSrvc.realname = function() {
 		var result = ConfigSrvc.CACHE.Account.allow_real_name;
 //		console.log('ConfigSrvc.realname() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	
 	ConfigSrvc.emailChange = function() {
 		var result = ConfigSrvc.CACHE.Account.allow_email_change;
 		console.log('ConfigSrvc.emailChange() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 
 	ConfigSrvc.emailFormatChange = function() {
 		var result = ConfigSrvc.CACHE.Account.allow_email_fmt_change;
 		console.log('ConfigSrvc.emailFormatChange() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 
 	ConfigSrvc.guestSettings = function() {
 		var result = ConfigSrvc.CACHE.Account.allow_guest_settings;
 		console.log('ConfigSrvc.guestSettings() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	ConfigSrvc.countryChange = function() {
 		var result = ConfigSrvc.CACHE.Account.allow_country_change;
 		console.log('ConfigSrvc.countryChange() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	ConfigSrvc.languageChange = function() {
 		var result = ConfigSrvc.CACHE.Account.allow_lang_change;
 		console.log('ConfigSrvc.languageChange() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	ConfigSrvc.birthdayChange = function() {
 		var result = ConfigSrvc.CACHE.Account.allow_birthday_change;
 		console.log('ConfigSrvc.birthdayChange() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 	ConfigSrvc.genderChange = function() {
 		var result = ConfigSrvc.CACHE.Account.allow_gender_change;
 		console.log('ConfigSrvc.genderChange() === ', result);
-		return result;
+		return ConfigSrvc.bool(result);
 	};
 
 	return ConfigSrvc;
